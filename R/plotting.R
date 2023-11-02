eb917c61a8993a6231fd664f497c4aa35ee35db0
Jian Yen <jdl.yen@gmail.com>
2023-11-02 21:31:33 +1100

    add additional plots by system with axes in natural units

--- R/plotting.R
+++ R/plotting.R
@@ -1,14 +1,13 @@
-
 # extract YCS/recruitment estimates (work out which this is)
-plot_flow_effects <- function(x, dat, average = FALSE, xname = NULL, rescale = NULL) {
-  
+plot_flow_effects <- function(x, dat, xname = NULL, rescale = NULL) {
+
   # reformat and tidy up names
   tmp <- x |>
-    mutate(
-      Waterbody = gsub("Gadopsis marmoratus|Gadopsis bispinosus|_sp", "", facet),
-      Species = sapply(strsplit(as.character(facet) ,"_sp"), \(x) x[2])
+    rename(
+      Waterbody = facet,
+      Species = group
     ) |>
-    filter(Species == group)
+    select(-conf.low, -conf.high)
   
   # rescale predictor if possible
   if (!is.null(rescale)) {
@@ -26,27 +25,28 @@ plot_flow_effects <- function(x, dat, average = FALSE, xname = NULL, rescale = N
     filter(catch > 0)
   
   # average over waterbodies if required
-  if (average) {
-    tmp <- tmp |>
-      group_by(x, Species) |>
-      summarise(predicted = mean(predicted))
-  } else {
-    tmp <- tmp |>
-      mutate(label = ifelse(x == max(x), Waterbody, NA))
-  }
+  ave <- tmp |>
+    group_by(x, Species) |>
+    summarise(predicted = mean(predicted)) |>
+    mutate(Waterbody = "Averaged")
+  tmp <- bind_rows(tmp, ave)
+  tmp <- tmp |>
+    mutate(
+      label = ifelse(x == max(x), Waterbody, NA),
+      label = ifelse(label == "Averaged", "", label)
+    )
+  
+  # set up a line width based on whether it's an average or waterbody value
+  tmp <- tmp |>
+    mutate(line_width = ifelse(Waterbody == "Averaged", 1.05, 1.03))
   
   # set up base plot, with wtaerbodies if needed
-  if (average) {
-    p <- tmp |>
-      ggplot(aes(y = predicted, x = x))
-  } else {
-    p <-   tmp |>
-      ggplot(aes(y = predicted, x = x, col = Waterbody))
-  }
+  p <-   tmp |>
+    ggplot(aes(y = predicted, x = x, col = Waterbody))
   
   # group by species
   p <- p +
-    geom_line(linewidth = 1.25) +
+    geom_line(aes(linewidth = line_width)) +
     facet_wrap( ~ Species, scales = "free") +
     ylab("Young-of-year CPUE") +
     ylim(0, NA)
@@ -56,15 +56,74 @@ plot_flow_effects <- function(x, dat, average = FALSE, xname = NULL, rescale = N
     p <- p + xlab(xname)
   
   # add legend if required
-  if (!average) {
-    p <- p + 
-      geom_label_repel(
-        aes(label = label),
-        nudge_x = 1,
-        na.rm = TRUE
-      ) +
-      theme(legend.position = "none")
+  p <- p + 
+    geom_label_repel(
+      aes(label = label),
+      nudge_x = 1,
+      na.rm = TRUE
+    ) +
+    theme(legend.position = "none")
+  
+  # return
+  p
+  
+}
+
+# function to plot system-level associations for each variable
+plot_flow_effects_system <- function(x, dat, metric_range, lt_vals, xname = NULL, rescale = NULL) {
+  
+  tmp <- x
+  
+  # rescale predictor if possible
+  if (!is.null(rescale)) {
+    tmp <- tmp |>
+      mutate(x = rescale[1] + rescale[2] * x)
   }
+  
+  # restrict to range observed in that system
+  tmp <- tmp |>
+    left_join(metric_range, by = c("Waterbody", "Reach")) |>
+    filter(x >= min_value, x <= max_value)
+  
+  # and rescale appropriately for the metric
+  tmp <- tmp |>
+    left_join(lt_vals, by = c("Waterbody", "Reach")) |>
+    mutate(x = x * scale_factor)
+  
+  # set up a filter based on predicted counts being largely greater than zero
+  include <- dat |> 
+    group_by(species, waterbody, reach_no) |> 
+    summarise(catch = sum(catch))
+  tmp <- tmp |>
+    left_join(include, by = c("Species" = "species", "Waterbody" = "waterbody", "Reach" = "reach_no")) |>
+    filter(catch > 0)
+  
+  # plot each waterbody in turn
+  waterbody_list <- tmp |>
+    distinct(Waterbody, Reach)
+  p <- vector("list", length = length(waterbody_list))
+  for (i in seq_len(nrow(waterbody_list))) {
+    
+    # set up base plot, with wtaerbodies if needed
+    p[[i]] <- tmp |>
+      filter(Waterbody == waterbody_list$Waterbody[i], Reach == waterbody_list$Reach[i]) |>
+      ggplot(aes(y = predicted, x = x))
+    
+    # group by species
+    p[[i]] <- p[[i]] +
+      geom_line() +
+      facet_wrap( ~ Species, scales = "free") +
+      ylab("Young-of-year CPUE") +
+      ylim(0, NA)
+    
+    # add xlab is known
+    if (!is.null(xname)) 
+      p[[i]] <- p[[i]] + xlab(xname)
+    
+  }
+  
+  # add some names so we know which is which
+  names(p) <- tolower(gsub(" ", "_", paste(waterbody_list$Waterbody, waterbody_list$Reach, sep = "_r")))
   
   # return
   p
--- main.R
+++ main.R
@@ -1,5 +1,9 @@
 # analysis of blackfish recruitment based on age-length estimates
 
+# Author: Jian Yen (jian.yen [at] delwp.vic.gov.au)
+# Created: August 2023
+# Last updated: 1 September 2023
+
 # load packages
 library(qs)
 library(readxl)
@@ -23,7 +27,7 @@ source("R/flow.R")
 source("R/plotting.R")
 
 # flags to refit model
-refit_model <- TRUE
+refit_model <- FALSE
 
 # need fish catches with age information (filtered to 0+ and 1+)
 cpue <- fetch_fish(recompile = FALSE)
@@ -57,8 +61,7 @@ cpue <- cpue |>
     flow |> select(date_formatted, waterbody, reach_no, stream_discharge_mld),
     by = c("survey_date" = "date_formatted", "waterbody", "reach_no")
   ) |>
-  rename(survey_flow = stream_discharge_mld) |>
-  mutate(survey_flow_std = scale(survey_flow))
+  rename(survey_flow = stream_discharge_mld)
 
 # flatten to a single observation per year
 cpue <- cpue |>
@@ -72,9 +75,12 @@ cpue <- cpue |>
     estimated_age
   ) |>
   summarise(
+    survey_flow = median(survey_flow),
     effort_h = sum(effort_h),
     catch = sum(catch)
-  )
+  ) |>
+  ungroup() |>
+  mutate(survey_flow_std = scale(survey_flow))
 cpue_ad <- cpue_ad |>
   group_by(
     id_site,
@@ -82,7 +88,8 @@ cpue_ad <- cpue_ad |>
     gear_type,
     scientific_name
   ) |>
-  summarise(catch = sum(catch))
+  summarise(catch = sum(catch)) |>
+  ungroup()
 
 # include adult_ym1 catch as a predictor (species-specific)
 cpue <- cpue |> 
@@ -96,7 +103,10 @@ cpue <- cpue |>
       rename(adult_catch_m1 = catch),
     by = c("id_site", "recruit_year_m1" = "survey_year", "gear_type", "scientific_name")
   ) |>
-  mutate(adult_catch_m1 = ifelse(is.na(adult_catch_m1), 0, adult_catch_m1))
+  mutate(
+    adult_catch_m1 = ifelse(is.na(adult_catch_m1), 0, adult_catch_m1),
+    log_adult_catch_m1 = log(adult_catch_m1 + 1)
+  )
 
 # attach flow to cpue data, aligning 1+ fish with flow in the previous year
 #    (the year in which they were spawned)
@@ -111,6 +121,7 @@ cpue <- cpue |>
 # standardise flow metrics
 cpue <- cpue |>
   mutate(
+    species = scientific_name,
     lowflow_wateryear_std = scale(lowflow_wateryear),
     dailyflow_spring_std = scale(dailyflow_spring),
     dailyflow_summer_std = scale(dailyflow_summer),
@@ -120,6 +131,110 @@ cpue <- cpue |>
     spawning_temperature_std = scale(spawning_temperature)
   )
 
+# plot flow conditions
+var_lookup <- c(
+  "cvflow_spawning" = "Flow variability",
+  "dailyflow_spring" = "Spring flow",
+  "dailyflow_summer" = "Summer flow",
+  "dailyflow_winter" = "Winter flow",
+  "lowflow_wateryear" = "Days of low flow",
+  "maxantecedent_wateryear" = "Maximum antecedent flow",
+  "spawning_temperature" = "Water temp. during spawning"
+)
+metrics_to_plot <- metrics |>
+  select(
+    waterbody, reach_no, water_year,
+    lowflow_wateryear, dailyflow_spring,
+    dailyflow_summer, dailyflow_winter,
+    maxantecedent_wateryear, cvflow_spawning,
+    spawning_temperature
+  ) |>
+  pivot_longer(
+    cols = c(contains("flow"), contains("temperature"), contains("wateryear")),
+    names_to = "metric"
+  ) |>
+  mutate(
+    waterbody_reach = paste(waterbody, reach_no, sep = ": Reach "),
+    metric = var_lookup[metric]
+  )
+
+p_coastal <- metrics_to_plot |>
+  filter(grepl("Glenelg|Moorabool|Thomson", waterbody)) |>
+  ggplot(aes(y = value, x = water_year, col = waterbody_reach)) +
+  geom_line() +
+  facet_wrap( ~ metric, scales = "free") +
+  scale_color_brewer(palette = "Set2", name = "") +
+  xlab("") +
+  theme(legend.position = "bottom") +
+  guides(colour = guide_legend(ncol = 3))
+p_inland <- metrics_to_plot |>
+  filter(!grepl("Glenelg|Moorabool|Thomson", waterbody)) |>
+  ggplot(aes(y = value, x = water_year, col = waterbody_reach)) +
+  geom_line() +
+  facet_wrap( ~ metric, scales = "free") +
+  scale_color_discrete(name = "") +
+  xlab("") +
+  theme(legend.position = "bottom") +
+  guides(colour = guide_legend(ncol = 3))
+p_var_coastal <- metrics_to_plot |>
+  filter(
+    grepl("Glenelg|Moorabool|Thomson", waterbody),
+    metric == "Flow variability"
+  ) |>
+  ggplot(aes(y = value, x = water_year)) +
+  geom_line() +
+  facet_wrap( ~ waterbody_reach) +
+  xlab("") +
+  theme(legend.position = "bottom")
+p_var_inland <- metrics_to_plot |>
+  filter(
+    !grepl("Glenelg|Moorabool|Thomson", waterbody),
+    metric == "Flow variability"
+  ) |>
+  ggplot(aes(y = value, x = water_year)) +
+  geom_line() +
+  facet_wrap( ~ waterbody_reach) +
+  xlab("") +
+  theme(legend.position = "bottom")
+
+# save to file
+ggsave(
+  file = "outputs/figures/flow-metrics-inland.png",
+  plot = p_inland,
+  device = ragg::agg_png,
+  width = 7,
+  height = 7,
+  units = "in",
+  res = 600
+)
+ggsave(
+  file = "outputs/figures/flow-metrics-coastal.png",
+  plot = p_coastal,
+  device = ragg::agg_png,
+  width = 7,
+  height = 7,
+  units = "in",
+  res = 600
+)
+ggsave(
+  file = "outputs/figures/flow-variability-coastal.png",
+  plot = p_var_coastal,
+  device = ragg::agg_png,
+  width = 7,
+  height = 7,
+  units = "in",
+  res = 600
+)
+ggsave(
+  file = "outputs/figures/flow-variability-inland.png",
+  plot = p_var_inland,
+  device = ragg::agg_png,
+  width = 7,
+  height = 7,
+  units = "in",
+  res = 600
+)
+
 # fit model or load saved model (set at top)
 if (refit_model) {
   
@@ -128,23 +243,29 @@ if (refit_model) {
   mod <- brm(
     bf(
       catch ~ estimated_age +
-        adult_catch_m1 +
+        log_adult_catch_m1 +
         survey_flow_std + 
         lowflow_wateryear_std +
-        poly(dailyflow_spring_std, 2) +
-        poly(dailyflow_summer_std, 2) +
+        dailyflow_spring_std + 
+        I(dailyflow_spring_std ^ 2) +
+        dailyflow_summer_std + 
+        I(dailyflow_summer_std ^ 2) +
         dailyflow_winter_std +
         maxantecedent_wateryear_std +
         cvflow_spawning_std +
-        poly(spawning_temperature_std, 2) +
+        spawning_temperature_std + 
+        I(spawning_temperature_std ^ 2) +
         (estimated_age +
            lowflow_wateryear_std +
-           poly(dailyflow_spring_std, 2) +
-           poly(dailyflow_summer_std, 2) +
+           dailyflow_spring_std + 
+           I(dailyflow_spring_std ^ 2) +
+           dailyflow_summer_std + 
+           I(dailyflow_summer_std ^ 2) +
            dailyflow_winter_std +
            maxantecedent_wateryear_std +
            cvflow_spawning_std +
-           poly(spawning_temperature_std, 2) | species) +
+           spawning_temperature_std + 
+           I(spawning_temperature_std ^ 2) | species) +
         (estimated_age +
            lowflow_wateryear_std +
            dailyflow_spring_std +
@@ -153,6 +274,7 @@ if (refit_model) {
            maxantecedent_wateryear_std +
            cvflow_spawning_std +
            spawning_temperature_std | species:waterbody) +
+        (1 | waterbody) +
         (1 | waterbody:reach_no) +
         (1 | species:waterbody:reach_no) +
         (1 | survey_year) +
@@ -177,10 +299,12 @@ if (refit_model) {
     chains = 4,
     cores = 4,
     seed = stan_seed,
-    iter = 2000,
-    warmup = 1000,
-    control = list(adapt_delta = 0.8, max_treedepth = 10),
-    backend = "rstan",
+    iter = 3000,
+    warmup = 2000,
+    control = list(adapt_delta = 0.9, max_treedepth = 15),
+    backend = "cmdstan",
+    refresh = 100,
+    silent = 0,
     threads = threading(3)
   )
   
@@ -290,7 +414,7 @@ flow_vars <- c(
 flow_effects <- vector("list", length = length(flow_vars))
 flow_effects <- lapply(
   flow_vars,
-  \(x) ggpredict(mod, terms = c(x, "species", "waterbody"), type = "random", ci.lvl = 0.5) |> as_tibble()
+  \(x) ggpredict(mod, terms = c(x, "species", "waterbody"), type = "random", ci.lvl = 0.5, allow_new_levels = TRUE) |> as_tibble()
 )
 
 # plot these
@@ -316,29 +440,122 @@ for (i in seq_along(flow_effects)) {
   p <- plot_flow_effects(
     flow_effects[[i]], 
     dat = cpue,
-    average = TRUE,
     xname = flow_vars_clean[i],
     rescale = rescale_values[[i]]
-  ) /
-    plot_flow_effects(
-      flow_effects[[i]], 
-      dat = cpue,
-      average = FALSE, 
-      xname = flow_vars_clean[i],
-      rescale = rescale_values[[i]]
-    ) +
-    plot_layout(heights = c(1, 3))
+  )
   ggsave(
     filename = paste0("outputs/figures/flow-", flow_vars[i], ".png"),
     plot = p,
+    device = ragg::agg_png,
+    width = 7,
+    height = 5,
+    dpi = 600,
+    units = "in"
+  )
+}
+
+# Plots for each system with ml/d as axes.
+min_vals <- metrics |>
+  select(-water_year) |>
+  group_by(waterbody, reach_no) |>
+  summarise(across(everything(), .fns = list(min = min), .names = "{.col}")) |>
+  mutate(type = "min")
+max_vals <- metrics |>
+  select(-water_year) |>
+  group_by(waterbody, reach_no) |>
+  summarise(across(everything(), .fns = list(max = max), .names = "{.col}")) |>
+  mutate(
+    type = "max",
+    lowflow_wateryear = ifelse(waterbody == "Holland Creek", 10, lowflow_wateryear),
+    lowflow_wateryear = ifelse(waterbody == "Glenelg River" & reach_no == 1, 10, lowflow_wateryear),
+    lowflow_wateryear = ifelse(waterbody == "Moorabool River" & reach_no == 4, 10, lowflow_wateryear)
+  )
+metrics_range <- bind_rows(min_vals, max_vals)
+
+# and calculate long-term median to rescale flow values
+lt_values <- flow |>
+  group_by(waterbody, reach_no) |>
+  summarise(
+    median = median(stream_discharge_mld),
+    mean = mean(stream_discharge_mld)
+  )
+
+# plot flow effects rescaled to their observation scale (e.g. ML/d)
+flow_vars_clean2 <- c(
+  "Number of days of flow below Q90",
+  "Average spring flow (ML/d)",
+  "Average summer flow (ML/d)",
+  "Average winter flow (ML/d)",
+  "Maximum antecedent flow (ML/d)",
+  "Std. dev. of flow during spawning season (ML/d)",
+  "Water temperature during spawning season (C)"
+)
+p <- vector("list", length = length(flow_effects))
+for (i in seq_along(flow_effects)) {
+  
+  # pull out range as select(Waterbody, min_value, max_value)
+  metrics_sub <- metrics_range |>
+    select(waterbody, reach_no, type, all_of(gsub("_std", "", flow_vars[i]))) |>
+    pivot_wider(id_cols = c(waterbody, reach_no), names_from = type, values_from = gsub("_std", "", flow_vars[i])) |>
+    rename(Waterbody = waterbody, Reach = reach_no, min_value = min, max_value = max) |>
+    mutate(Reach = as.character(Reach))
+  
+  # and lt_values as select(Waterbody, scale_factor)
+  lt_sub <- lt_values |>
+    mutate(
+      scale_factor = median,
+      scale_factor = ifelse(grepl("temperature|lowflow", flow_vars[i]), 1, scale_factor),
+      scale_factor = ifelse(grepl("cvflow", flow_vars[i]), mean, scale_factor)
+    ) |>
+    rename(Waterbody = waterbody, Reach = reach_no)
+  
+  # exand and include reaches in this output
+  flow_sub <- full_join(
+    flow_effects[[i]] |>
+      select(-conf.low, -conf.high) |>
+      rename(Species = group, Waterbody = facet),
+    lt_sub |> distinct(Waterbody, Reach),
+    by = c("Waterbody"),
+    relationship = "many-to-many"
+  )
+  
+  # and plot this
+  p[[i]] <- plot_flow_effects_system(
+    x = flow_sub, 
+    dat = cpue |> mutate(reach_no = as.character(reach_no)),
+    metric_range = metrics_sub,
+    lt_vals = lt_sub,
+    xname = flow_vars_clean2[i],
+    rescale = rescale_values[[i]]
+  )
+  
+}
+
+# make up a plot for each system (all variables)
+all_systems <- lt_values |> 
+  mutate(waterbody_reach = tolower(gsub(" ", "_", paste(waterbody, reach_no, sep = "_r")))) |>
+  pull(waterbody_reach) |>
+  unique()
+for (i in seq_along(all_systems)) {
+  
+  flow_plots <- lapply(p, \(x, sys) x[[sys]], sys = all_systems[i])
+  sys_plot <- (flow_plots[[1]] + flow_plots[[2]]) /
+    (flow_plots[[3]] + flow_plots[[4]]) /
+    (flow_plots[[5]] + flow_plots[[6]]) /
+    (flow_plots[[7]] + plot_spacer())
+  ggsave(
+    filename = paste0("outputs/figures/flow-", all_systems[i], ".png"),
+    plot = sys_plot,
     device = ragg::agg_png,
     width = 8,
     height = 8,
     dpi = 600,
     units = "in"
   )
+  
 }
 
+# extract YCS/recruitment estimates (work out which this is)
 # plot recruitment
 newdat <- cpue |>
   group_by(waterbody, reach_no, survey_year, species) |>
